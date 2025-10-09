fb5fd73b0eb7e564b1580baddbd3c852efd6cca8
2alheure <jojo77.juv@live.fr>
2025-10-09 08:41:57 +0200

    ID dans l'URL

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class ArticleController extends AbstractController {
     #[Route('/article/create', name: 'app_article')]
@@ -28,6 +29,41 @@ final class ArticleController extends AbstractController {
 
         return $this->render('article/create.html.twig', [
             'form' => $form
+        ]);
+    }
+
+    #[Route('/articles/{id}/update', name: 'app_article_update')]
+    public function update($id, ArticleRepository $ar, Request $request, EntityManagerInterface $em): Response {
+        // J'utilise l'ID de l'URL pour récupérer l'article en BDD
+        $article = $ar->find($id);
+        if (!$article) {
+            throw new NotFoundHttpException("L'article $id n'existe pas");
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('app_article_liste');
+        }
+
+        return $this->render('article/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/articles/{id}', name: 'app_article_detail')]
+    public function detail(Article $article): Response {
+
+        // Symfony va automatiquement récupérer l'article en BDD
+        // grâce à l'ID dans l'URL et l'injection de dépendance
+        // Si l'article n'existe pas, une 404 sera automatiquement générée
+
+        return $this->render('article/details.html.twig', [
+            'article' => $article
         ]);
     }
 
